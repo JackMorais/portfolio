b4954bf9d16efefe3f2e1ba93c971b085d1b3018
Jackson Morais <jack_h-22@hotmail.com>
2024-10-19 10:57:02 -0300

    construindo a estrutura e a seção "sobre mim"

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,9 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/JackMorais.png" />
+    <Titulo fontSize={20}>Jackson Morais</Titulo>
+  </aside>
+)
+export default Sidebar
