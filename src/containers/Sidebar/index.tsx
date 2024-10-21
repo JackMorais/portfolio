bc300a67d506d60891397b51c9d877c2963db2c2
Jackson Morais <jack_h-22@hotmail.com>
2024-10-21 20:03:28 -0300

    estilizando o sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,20 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/JackMorais.png" />
-    <Titulo fontSize={20}>Jackson Morais</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={17}>JacksonMorais</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        JackMorais
+      </Paragrafo>
+      <Descricao fontSize={12}>Engenheiro Front-End</Descricao>
+      <BotaoTema>Trocar Tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 export default Sidebar
